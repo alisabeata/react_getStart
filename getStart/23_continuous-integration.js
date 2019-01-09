ff0a88c5c6950ebbbd1bbf2e0a4fd3eb3df0d8da
alisabeata <alisa314159@gmail.com>
2019-01-10 00:32:57 +0200

    lesson 11.1 testing

--- getStart/23_continuous-integration.js
+++ getStart/23_continuous-integration.js
@@ -3,3 +3,38 @@
 // Travis ci
 // https://travis-ci.org/
 // доплонительное тестирование на внешнем сервере
+// Travis работает на докере
+// есть возможность отправлять логи, например, в чат слака
+
+// https://circleci.com/ аналог Travis
+
+// https://www.netlify.com/ free хостинг
+
+
+// in .travis.yml
+
+language: node_js
+node_js:
+  -8
+  
+cach: yarn
+script:
+  // описание shell команд
+  - yarn build
+  - yarn test
+  
+  
+// другие доступные методы
+after_install: 
+before_install: 
+install: 
+after_script: 
+before_script: 
+after_success: 
+after_failure: 
+
+after_success: 
+  notifications:
+    - slack
+    - mail
+    
