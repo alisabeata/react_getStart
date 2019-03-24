645b0147dafa786280043f18022720a17eb20c6b
alisabeata <alisa314159@gmail.com>
2019-03-24 15:38:05 +0200

    ci

--- getStart/24_continuous-integration.js
+++ getStart/24_continuous-integration.js
@@ -1,28 +1,48 @@
 // continuous integration
 
-// Travis ci
+
+// этапы:
+// - запуск тестов
+// - доставка билда на сервер
+// - вывод статистики по коду
+
+
+
+// - Travis CI
+// доступен в беспл режиме только для паблик репозиториев на гитхаб
 // https://travis-ci.org/
 // доплонительное тестирование на внешнем сервере
 // Travis работает на докере
 // есть возможность отправлять логи, например, в чат слака
 
+// - Circle CI
 // https://circleci.com/ аналог Travis
+// беспл режим для огр кол-ва проектов
 
 // https://www.netlify.com/ free хостинг
 // https://www.scaleway.com/ дешёвый хостинг
 
 
-// in .travis.yml
-
+// .travis.yml
 language: node_js
 node_js:
-  -8
+  - 9
   
-cach: yarn
+cache: 
+  directories:
+    - ~/.npm
+    - ~/.cache
+    
+install:
+  - yarn install
+    
 script:
   // описание shell команд
   - yarn build
   - yarn test
+  // вполне можно дописать любую команду для линукса
+  // rm build/static/js/*.js.map   напр удаление сорсмапов
+  
   
   
 // другие доступные методы
@@ -39,3 +59,16 @@ after_success:
     - slack
     - mail
     
+    
+// деплой на хероку
+// https://www.heroku.com/
+    
+// .travis.yml
+...
+
+deploy:
+  provider: heroku
+  api_key:
+    secure: $HEROKU_KEY // $HEROKU_KEY переменная среды, указ в настр travis
+  app: name-project
+  
