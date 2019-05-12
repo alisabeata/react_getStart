5fb6c69148564e702fa46e2bd92671a4ccf34815
alisabeata <alisa314159@gmail.com>
2019-05-12 13:51:57 +0300

    lazy-loading

--- getStart/32_lazy-loading.js
+++ getStart/32_lazy-loading.js
@@ -5,7 +5,7 @@
 // https://reactjs.org/docs/code-splitting.html
 // https://github.com/smooth-code/loadable-components
 
-
+// нативная реализация, на текущий момент не поддерж северсайд рендеринг
 import React, {Component, lazy, Suspense} from 'react';
 
 const OtherComponent = React.lazy(() => import('./OtherComponent'));
@@ -18,4 +18,21 @@ function MyComponent() {
       </Suspense>
     </div>
   );
+}
+
+
+// - react-loadable
+// https://github.com/jamiebuilds/react-loadable
+import Loadable from 'react-loadable';
+import Loading from './my-loading-component';
+
+const LoadableComponent = Loadable({
+  loader: () => import('./my-component'),
+  loading: Loading,
+});
+
+export default class App extends React.Component {
+  render() {
+    return <LoadableComponent />;
+  }
 }
