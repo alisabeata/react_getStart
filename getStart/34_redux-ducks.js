11156455b752a8b31895ba69e0dddd422d9cac6d
alisabeata <alisa314159@gmail.com>
2019-09-25 15:19:10 +0300

    ducks

--- getStart/34_redux-ducks.js
+++ getStart/34_redux-ducks.js
@@ -29,3 +29,16 @@ export function signUp(email, password) {
     console.log('----', 'sign up');
   };
 }
+
+
+// in ../reducer.js
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import authReducer, { moduleName as authModule } from '../ducks/auth';
+
+export default history =>
+  combineReducers({
+    router: connectRouter(history),
+    // rest of your reducers
+    [authModule]: authReducer
+  });
