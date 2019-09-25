5e05537b01e01b77e757846b56f7aba45a0e87a5
alisabeata <alisa314159@gmail.com>
2019-09-25 15:18:12 +0300

    ducks

---
+++ getStart/34_redux-ducks.js
@@ -0,0 +1,31 @@
+// ducks-modular-redux
+// https://github.com/erikras/ducks-modular-redux
+
+// способ организации кода
+
+
+// in ducks/auth.js
+import { appName } from '../config';
+
+// const
+export const moduleName = 'auth';
+export const SIGN_UP_REQUEST = `${appName}/${moduleName}/SIGN_UP_REQUEST`;
+export const SIGN_UP_SUCCESS = `${appName}/${moduleName}/SIGN_UP_SUCCESS`;
+export const SIGN_UP_ERROR = `${appName}/${moduleName}/SIGN_UP_ERROR`;
+
+// reducer
+export default function reducer(state, action) {
+  const { type } = action;
+
+  switch (type) {
+    default:
+      return state;
+  }
+}
+
+// action creator (with thunk)
+export function signUp(email, password) {
+  return dispatch => {
+    console.log('----', 'sign up');
+  };
+}
